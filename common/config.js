8d1bbfd5b32e3f44156b5fbda4cf4a4f479bb80c
Stanislav Simovski <megakoresh@gmail.com>
2017-11-10 10:25:57 +0200

    Remove Database dependency from config.js
    Configuration must be fully loaded before everything else, so it should never require any other modules from the app other than commons

--- common/config.js
+++ common/config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const { MongoDatabase, MariaDatabase } = require('database');
 
 module.exports = {
   csrf: {
@@ -14,7 +13,5 @@ module.exports = {
     session: process.env['SESSION_SECRET'] || 'session-secret'    
   },
   appRoot: __dirname.split(path.sep).slice(0,-1).join(path.sep),
-  env: process.env['NODE_ENV'],
-  defaultMongo: new MongoDatabase(encodeURI(`mongodb://${process.env['MONGO_TEST_USER']}:${process.env['MONGO_TEST_PASS']}@${process.env['MONGO_TEST_HOST']}/testdb`)),
-  defaultMaria: new MariaDatabase(encodeURI(`mysql://${process.env['MARIA_TEST_USER']}:${process.env['MARIA_TEST_PASS']}@${process.env['MARIA_TEST_HOST']}:3306/test`))
+  env: process.env['NODE_ENV']
 }
